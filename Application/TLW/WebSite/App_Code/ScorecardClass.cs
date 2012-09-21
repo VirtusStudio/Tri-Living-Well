ed3880ad0eef434eacf666d9a98e9a1abda65340
David Bowers <david@dajomasoft.net>
2012-09-21 03:11:50 -0400

    supporting class for scorecard popup

--- Application/TLW/WebSite/App_Code/ScorecardClass.cs
+++ Application/TLW/WebSite/App_Code/ScorecardClass.cs
@@ -14,81 +14,23 @@ using System.Web.UI.HtmlControls;
 /// </summary>
 public class ScorecardClass
 {
-    #region PrivateMembers
-    private string m_sUserID;
-    private string m_sDateSelected;
-    private int m_iPersonalSummaryIdFirst;
-    private decimal m_decWeightFirst;
-    private decimal m_decWaistFirst;
-    private decimal m_decBMIFirst;
-    private decimal m_decBodyFatPercentageFirst;
-    private decimal m_decWaistToHeightFirst;
-    private decimal m_decWaistToHipFirst;
-    private int m_iPersonalSummaryIdLast;
-    private decimal m_decWeightLast;
-    private decimal m_decWaistLast;
-    private decimal m_decBMILast;
-    private decimal m_decBodyFatPercentageLast;
-    private decimal m_decWaistToHeightLast;
-    private decimal m_decWaistToHipLast;
     private SqlConnection MyConnection;
     private SqlCommand MyCommand;
     private SqlDataAdapter MyDataAdapter;
-    private DataSet MyDataSet = new DataSet();
-    #endregion
 
-    #region Constructors
-    public ScorecardClass(SqlConnection objSQLConnection, string sUserID, string sDateSelected = "")
+    public ScorecardClass(SqlConnection objSQLConnection)
 	{
-        m_iPersonalSummaryIdFirst = 0;
-        m_decWeightFirst = 0m;
-        m_decWaistFirst = 0m;
-        m_decBMIFirst = 0m;
-        m_decBodyFatPercentageFirst = 0m;
-        m_decWaistToHeightFirst = 0m;
-        m_decWaistToHipFirst = 0m;
-        m_iPersonalSummaryIdLast = 0;
-        m_decWeightLast = 0m;
-        m_decWaistLast = 0m;
-        m_decBMILast = 0m;
-        m_decBodyFatPercentageLast = 0m;
-        m_decWaistToHeightLast = 0m;
-        m_decWaistToHipLast = 0m;
-
         try
         {
-            m_sUserID = sUserID;
-            m_sDateSelected = sDateSelected;
             MyConnection = objSQLConnection;
         }
         catch (Exception ex)
         {
             HttpContext.Current.Response.Write(ex.Message);
         }
-        GetUserData();
 	}
-    #endregion
 
-    #region Public Methods
-    public string GetUserID() { return m_sUserID; }
-    public int GetPersonalSummaryIdFirst() { return m_iPersonalSummaryIdFirst; }
-    public decimal GetWeightFirst() { return m_decWeightFirst; }
-    public decimal GetWaistFirst() { return m_decWaistFirst; }
-    public decimal GetBMIFirst() { return m_decBMIFirst; }
-    public decimal GetBodyFatPercentageFirst() { return m_decBodyFatPercentageFirst; }
-    public decimal GetWaistToHeightFirst() { return m_decWaistToHeightFirst; }
-    public decimal GetWaistToHipFirst() { return m_decWaistToHipFirst; }
-    public int GetPersonalSummaryIdLast() { return m_iPersonalSummaryIdLast; }
-    public decimal GetWeightLast() { return m_decWeightLast; }
-    public decimal GetWaistLast() { return m_decWaistLast; }
-    public decimal GetBMILast() { return m_decBMILast; }
-    public decimal GetBodyFatPercentageLast() { return m_decBodyFatPercentageLast; }
-    public decimal GetWaistToHeightLast() { return m_decWaistToHeightLast; }
-    public decimal GetWaistToHipLast() { return m_decWaistToHipLast; }
-    public string GetDateSelected() { return m_sDateSelected; }
-    public void SetDateSelected(string sDateSelected) { m_sDateSelected = sDateSelected; }
-
-    public void SetUserData( string sUserID, string sDateSelected, int iPersonalSummaryId, decimal decWeight, decimal decWaist, decimal decBMI, decimal decBodyFatPercentage, decimal decWaistToHeight, decimal decWaistToHip)
+    public void SetScorecard( string sUserID, string sDateSelected, int iPersonalSummaryId, decimal decWeight, decimal decWaist, decimal decBMI, decimal decBodyFatPercentage, decimal decWaistToHeight, decimal decWaistToHip)
     {
         SqlCommand MyCommand;
         try
@@ -110,17 +52,14 @@ public class ScorecardClass
         }
         catch (Exception ex)
         {
-            // TODO: log the error
+            HttpContext.Current.Response.Write(ex.Message);
         }
     }
 
-    #endregion
-
-    #region Private Methods
-
-    private void GetUserData()
+    public DataSet GetFirstScorecard(string sUserID, string sDateSelected)
     {
-        if (m_sUserID.Trim().Length == 0) return;
+        DataSet MyDataSet = new DataSet();
+        if (sUserID.Trim().Length == 0) return MyDataSet;
 
         try
         {
@@ -128,34 +67,22 @@ public class ScorecardClass
             MyCommand.Connection = MyConnection;
             MyCommand.CommandType = CommandType.StoredProcedure;
             MyCommand.CommandText = "spNutritionScorecardGetFirst";
-            MyCommand.Parameters.AddWithValue("@USER_ID", m_sUserID);
+            MyCommand.Parameters.AddWithValue("@USERID", sUserID);
             MyDataAdapter = new SqlDataAdapter(MyCommand);
             MyDataAdapter.Fill(MyDataSet);
-            if (MyDataSet.Tables[0].Rows.Count > 0)
-            {
-                m_iPersonalSummaryIdFirst = (Convert.IsDBNull(MyDataSet.Tables[0].Rows[0]["intPersonalSummaryId"])) ? 0 : Convert.ToInt32(MyDataSet.Tables[0].Rows[0]["intPersonalSummaryId"]);
-                m_decWeightFirst = (Convert.IsDBNull(MyDataSet.Tables[0].Rows[0]["decWeight"])) ? 0 : Convert.ToDecimal(MyDataSet.Tables[0].Rows[0]["decWeight"]);
-                m_decWaistFirst = (Convert.IsDBNull(MyDataSet.Tables[0].Rows[0]["decWaist"])) ? 0 : Convert.ToDecimal(MyDataSet.Tables[0].Rows[0]["decWaist"]);
-                m_decBMIFirst = (Convert.IsDBNull(MyDataSet.Tables[0].Rows[0]["decBMI"])) ? 0 : Convert.ToDecimal(MyDataSet.Tables[0].Rows[0]["decBMI"]);
-                m_decBodyFatPercentageFirst = (Convert.IsDBNull(MyDataSet.Tables[0].Rows[0]["decBodyFATPercentage"])) ? 0 : Convert.ToDecimal(MyDataSet.Tables[0].Rows[0]["decBodyFATPercentage"]);
-                m_decWaistToHeightFirst = (Convert.IsDBNull(MyDataSet.Tables[0].Rows[0]["decWaistToHeight"])) ? 0 : Convert.ToDecimal(MyDataSet.Tables[0].Rows[0]["decWaistToHeight"]);
-                m_decWaistToHipFirst = (Convert.IsDBNull(MyDataSet.Tables[0].Rows[0]["decWaistToHip"])) ? 0 : Convert.ToDecimal(MyDataSet.Tables[0].Rows[0]["decWaistToHip"]);
-            }
-            else
-            {
-                m_iPersonalSummaryIdFirst = 0;
-                m_decWeightFirst = 0m;
-                m_decWaistFirst = 0m;
-                m_decBMIFirst = 0m;
-                m_decBodyFatPercentageFirst = 0m;
-                m_decWaistToHeightFirst = 0m;
-                m_decWaistToHipFirst = 0m;
-            }
         }
         catch (Exception ex)
         {
-            // TODO: log the error
+            HttpContext.Current.Response.Write(ex.Message);
         }
+        return MyDataSet;
+    }
+
+
+    public DataSet GetLastScorecard(string sUserID, string sDateSelected)
+    {
+        DataSet MyDataSet = new DataSet();
+        if (sUserID.Trim().Length == 0) return MyDataSet;
 
         try
         {
@@ -163,40 +90,16 @@ public class ScorecardClass
             MyCommand.Connection = MyConnection;
             MyCommand.CommandType = CommandType.StoredProcedure;
             MyCommand.CommandText = "spNutritionScorecardGetLast";
-            MyCommand.Parameters.AddWithValue("@USER_ID", m_sUserID);
-            MyCommand.Parameters.AddWithValue("@DATE_SELECTED", m_sDateSelected);
+            MyCommand.Parameters.AddWithValue("@USERID", sUserID);
             MyDataAdapter = new SqlDataAdapter(MyCommand);
             MyDataAdapter.Fill(MyDataSet);
-            if (MyDataSet.Tables[0].Rows.Count > 0)
-            {
-                m_iPersonalSummaryIdLast = (Convert.IsDBNull(MyDataSet.Tables[0].Rows[0]["intPersonalSummaryId"])) ? 0 : Convert.ToInt32(MyDataSet.Tables[0].Rows[0]["intPersonalSummaryId"]);
-                m_decWeightLast = (Convert.IsDBNull(MyDataSet.Tables[0].Rows[0]["decWeight"])) ? 0 : Convert.ToDecimal(MyDataSet.Tables[0].Rows[0]["decWeight"]);
-                m_decWaistLast = (Convert.IsDBNull(MyDataSet.Tables[0].Rows[0]["decWaist"])) ? 0 : Convert.ToDecimal(MyDataSet.Tables[0].Rows[0]["decWaist"]);
-                m_decBMILast = (Convert.IsDBNull(MyDataSet.Tables[0].Rows[0]["decBMI"])) ? 0 : Convert.ToDecimal(MyDataSet.Tables[0].Rows[0]["decBMI"]);
-                m_decBodyFatPercentageLast = (Convert.IsDBNull(MyDataSet.Tables[0].Rows[0]["decBodyFATPercentage"])) ? 0 : Convert.ToDecimal(MyDataSet.Tables[0].Rows[0]["decBodyFATPercentage"]);
-                m_decWaistToHeightLast = (Convert.IsDBNull(MyDataSet.Tables[0].Rows[0]["decWaistToHeight"])) ? 0 : Convert.ToDecimal(MyDataSet.Tables[0].Rows[0]["decWaistToHeight"]);
-                m_decWaistToHipLast = (Convert.IsDBNull(MyDataSet.Tables[0].Rows[0]["decWaistToHip"])) ? 0 : Convert.ToDecimal(MyDataSet.Tables[0].Rows[0]["decWaistToHip"]);
-            }
-            else
-            {
-                m_iPersonalSummaryIdLast = 0;
-                m_decWeightLast = 0m;
-                m_decWaistLast = 0m;
-                m_decBMILast = 0m;
-                m_decBodyFatPercentageLast = 0m;
-                m_decWaistToHeightLast = 0m;
-                m_decWaistToHipLast = 0m;
-            }
         }
         catch (Exception ex)
         {
-            // TODO: log the error
+            HttpContext.Current.Response.Write(ex.Message);
         }
+        return MyDataSet;
     }
-
-    #endregion
-
-
 
 
 
